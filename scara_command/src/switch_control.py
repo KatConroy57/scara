e861070c016cacbe4f2f97a0331193939ae08941
joeyzhong90595 <zzy905954450@gmail.com>
2019-12-10 17:23:30 -0500

    Update launch

--- scara_command/src/switch_control.py
+++ scara_command/src/switch_control.py
@@ -38,7 +38,7 @@ def handle_switch(req):
 
     
 def switch_control():
-    rospy.init_node('switch_controller')
+    rospy.init_node('switch_control_node')
     s = rospy.Service('switch_control', SwitchControl, handle_switch)
 
     rospy.spin()
